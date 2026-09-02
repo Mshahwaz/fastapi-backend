3db1c299ec959837f5bbfd397e9ae2aa8103e6ca
Mohd Shahwaz <91405255+Mshahwaz@users.noreply.github.com>
2026-09-02 21:33:49 +0530

    Created Resuable authorisation and authenciation(admin or user ) dependencies

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ from datetime import datetime, timedelta, timezone
 from jose import jwt, JWTError
 from pwdlib import PasswordHash
 
+#Required Parameters in JWT token creation
+SECRET_KEY="my-super-secret-key"
+ALGORITHM="HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES=30
+
 password_hash = PasswordHash.recommended()
 # use it as : hashed_password = password_hash.hash("password")
 app=FastAPI()
@@ -15,7 +20,7 @@ security=HTTPBearer()
 
 DATABASE_URL=(
     "postgresql+psycopg://backend-svc-user:"
-    "admin@localhost:5432/backend_db"
+    "backend-svc-pass@localhost:5432/backend_db"
 )
 
 engine=create_engine(DATABASE_URL)
@@ -35,6 +40,7 @@ class User(SQLModel, table=True):
     age: int 
     username: str
     password: str
+    role: str = "user"
 
 #RESPONSE MODEL for client query response
 class UserResponse(SQLModel):
@@ -83,7 +89,8 @@ def register_user(user: User_create):
             name = user.name,
             age = user.age,
             username = user.username,
-            password = hashed_password
+            password = hashed_password,
+            role = "user"
         ) #creating DB user obj mapped to user table
         session.add(db_user)
         session.commit()
@@ -127,7 +134,7 @@ def get_user(user_id: int):
 def update_user(userobj: User_create,user_id: int):
     with Session(engine) as session:
         #Business Logic
-        if userobj.age > 18:
+        if userobj.age < 18:
             raise HTTPException(
                 status_code=400,
                 detail="User must be al least 18 years"
@@ -178,73 +185,6 @@ def patch_user(userobj: UserUpdate,user_id: int):
         session.refresh(user)
         return user
 
-
-#DELETE USER with user id
-@app.delete("/users/{user_id}")
-def del_user(user_id: int):
-    with Session(engine) as session:
-        user=session.get(User, user_id)
-        if not user:
-            raise HTTPException(
-                status_code=404,
-                detail=f"User not found with id {user_id}"
-            )
-        session.delete(user)
-        session.commit()
-        return {
-            "message" : f"User with id {user_id} has been removed successfully"
-        }
-#######################################
-
-SECRET_KEY="my-super-secret-key"
-ALGORITHM="HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES=30
-
-#Login Authentication
-@app.post(
-    "/login",status_code=status.HTTP_200_OK
-    )
-def login(login_data: LoginRequest):
-    ##################### Actual DB query User authentication ######################
-    with Session(engine) as session:
-        statement=select(User).where(
-            User.username == login_data.username
-        )
-        db_user= session.exec(statement).first()
-        if db_user is None:
-            raise HTTPException(
-                status_code=401,
-                detail="Invalid Username or password"
-            )
-        if not password_hash.verify(
-            login_data.password,
-            db_user.password
-        ):
-            raise HTTPException(
-                status_code=401,
-                detail="Invalid Username or password"
-            )
-    #debug
-    # print(f"user applied hash -{password_hash.hash(login_data.password)}")
-    # print(f" DB stored Hash - {db_user.password}")
-    #################################################
-    expire=datetime.now(timezone.utc)+timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    payload={
-        "sub":db_user.username,
-        "exp":expire
-    }
-    token =jwt.encode(
-        payload,
-        SECRET_KEY,
-        algorithm=ALGORITHM
-    )
-    return {
-        "message" : "Login Successfull",
-        # "access_token":"abc123", #fake token will used in authorization header for user authorization after login
-        "access_token":token, #Generated JWT token
-        "token_type":"bearer" 
-    }
-
 #Resuable Authenticatin dependency (This fn is responsible for user authentication)
 def get_current_user(
 credentials: HTTPAuthorizationCredentials = Depends(security)
@@ -284,7 +224,91 @@ credentials: HTTPAuthorizationCredentials = Depends(security)
             )
         return db_user
 
-#Protected Endpoint (Accessible only for authenticated User)  
+#Resuable admin verification dependency (This fn is responsible for admin authorization)
+def require_admin(
+    current_user: User = Depends(get_current_user)
+    ):
+        ########### Check if a user is a admin or normal user ################ 
+    if current_user.role != "admin":
+        raise HTTPException(
+            status_code=403,
+            detail="Admin Access required"
+        )
+    return current_user
+    ####################################################################       
+
+#DELETE USER with user id (Protected Endpoint only admin role can delete a user)
+@app.delete("/users/{user_id}")
+def del_user(user_id: int,current_user: User = Depends(require_admin)):
+    # ########### Check if a user is a admin or normal user ################ -- Handeled by require_admin fn
+    # if current_user.role != "admin":
+    #     raise HTTPException(
+    #         status_code=403,
+    #         detail="Admin Access required"
+    #     )
+    # ####################################################################
+    with Session(engine) as session:
+        user=session.get(User, user_id)
+        if not user:
+            raise HTTPException(
+                status_code=404,
+                detail=f"User not found with id {user_id}"
+            )
+        session.delete(user)
+        session.commit()
+        return {
+            "message" : f"User with id {user_id} has been removed successfully"
+        }
+#######################################
+
+#Login Authentication
+@app.post(
+    "/login",status_code=status.HTTP_200_OK
+    )
+def login(login_data: LoginRequest):
+    ##################### Actual DB query User authentication ######################
+    with Session(engine) as session:
+        statement=select(User).where(
+            User.username == login_data.username
+        )
+        db_user= session.exec(statement).first()
+        if db_user is None:
+            raise HTTPException(
+                status_code=401,
+                detail="Invalid Username or password"
+            )
+        if not password_hash.verify(
+            login_data.password,
+            db_user.password
+        ):
+            raise HTTPException(
+                status_code=401,
+                detail="Invalid Username or password"
+            )
+    #debug
+    # print(f"user applied hash -{password_hash.hash(login_data.password)}")
+    # print(f" DB stored Hash - {db_user.password}")
+    #################################################
+    expire=datetime.now(timezone.utc)+timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    payload={
+        "sub":db_user.username,
+        "role": db_user.role,
+        "exp":expire
+    }
+    token =jwt.encode(
+        payload,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+    return {
+        "message" : "Login Successfull",
+        # "access_token":"abc123", #fake token will used in authorization header for user authorization after login
+        "access_token":token, #Generated JWT token
+        "token_type":"bearer" 
+    }
+
+
+#Protected Endpoint (Accessible only for authenticated Users (anyone))
 @app.get("/protected")
 def protected_route(
     current_user: User = Depends(get_current_user)
@@ -293,5 +317,16 @@ def protected_route(
         "message":"You are authenticated",
         "username":current_user.username,
         "user_id":current_user.id,
-        "name": current_user.name
+        "name": current_user.name,
+        "role":current_user.role
+    }
+
+#Protected admin dashboard (accessible to admins only)
+@app.get("/admin/dashboard")
+def admin_dashboard(
+    current_user: User = Depends(require_admin)
+    ):
+    return {
+        "message" : " Welcome to Admin Dashboard ",
+        "user": current_user.name
     }
